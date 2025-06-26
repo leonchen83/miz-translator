821a76ba0a811d87bbe64da6d43b73af038d8929
leonchen83 <chen.bao.yi@qq.com>
2025-06-26 20:28:53 +0800

    init

--- src/main/java/org/example/impl/AbstractTranslator.java
+++ src/main/java/org/example/impl/AbstractTranslator.java
@@ -55,17 +55,31 @@ public abstract class AbstractTranslator implements Translator {
 	
 	@Override
 	public List<Map.Entry<String, String>> translates(List<Map.Entry<String, String>> texts, Map<String, String> options) {
+		if (texts.size() < 4) {
+			logger.warn("Less than 4 texts to translate, fallback to individual translation. texts:{}", texts);
+			return fallbackTranslates(texts, options);
+		}
 		List<String> values = texts.stream().map(Map.Entry::getValue).toList();
 		String joinedText = String.join(SPLITER, values);
 		String translatedText = translate(joinedText, null);
 		String[] parts = translatedText.split(SPLITER);
 		if (parts.length != values.size()) {
-			logger.warn("Translated text parts count {} does not match original values count {}. fallback to individual translation.", parts.length, values.size());
-			List<Map.Entry<String, String>> r = new ArrayList<>(texts.size());
-			for (Map.Entry<String, String> entry : texts) {
-				r.add(Map.entry(entry.getKey(), translate(entry.getValue(), options)));
+			logger.warn("Translated text parts count {} does not match original values count {}. split translation", parts.length, values.size());
+			int half = values.size() / 2;
+			List<Map.Entry<String, String>> list1 = new ArrayList<>(half);
+			List<Map.Entry<String, String>> list2 = new ArrayList<>(texts.size() - half);
+			for (int i = 0; i < half; i++) {
+				list1.add(texts.get(i));
 			}
-			return r;
+			for (int i = half; i < texts.size(); i++) {
+				list2.add(texts.get(i));
+			}
+			List<Map.Entry<String, String>> t1 = translates(list1, options);
+			List<Map.Entry<String, String>> t2 = translates(list2, options);
+			List<Map.Entry<String, String>> combined = new ArrayList<>(t1.size() + t2.size());
+			combined.addAll(t1);
+			combined.addAll(t2);
+			return combined;
 		}
 		return texts.stream()
 				.map(entry -> {
@@ -74,5 +88,13 @@ public abstract class AbstractTranslator implements Translator {
 					return Map.entry(entry.getKey(), parts[index]);
 				})
 				.toList();
+	}
+	
+	private List<Map.Entry<String, String>> fallbackTranslates(List<Map.Entry<String, String>> texts, Map<String, String> options) {
+		List<Map.Entry<String, String>> r = new ArrayList<>(texts.size());
+		for (Map.Entry<String, String> entry : texts) {
+			r.add(Map.entry(entry.getKey(), translate(entry.getValue(), options)));
+		}
+		return r;
 	}
 }
